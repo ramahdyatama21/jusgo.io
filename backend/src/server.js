6afdeb2e432b5ccc9817f9ceb9fef7ed8f9c030a
ramahdyatama21 <ramahdyatama17@gmail.com>
2025-10-01 11:25:09 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://jusgo-io.vercel.app', 'http://localhost:5173'], // tambahkan origin frontend
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
